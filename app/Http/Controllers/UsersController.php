f350659765f7ae621f4ca01b8f9d553a098cdf4a
Itzhak Franco <itzhakfranco@gmail.com>
2021-02-02 17:22:39 +0200

    first commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Users;
+
+class UsersController extends Controller
+{
+    public function getUsers(){
+       return response()->json(Users::all());
+    }
+    
+    public function addUser(Request $request){
+       $user = Users::addUser($request);
+      return response($user); 
+    }
+
+    public function updateUser(Request $request,$id){
+  
+      $user =  Users::updateUser($request,$id);
+      return response($user); 
+
+
+   }
+    public function deleteUser(Request $request,$id){
+  
+      Users::deleteUser($request,$id);
+      return response()->json(null,204); 
+
+
+   }
+}
---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+
+Route::prefix('api')->group(function () {
+Route::get('users', [UsersController::class, 'getUsers']);
+Route::post('addUser', [UsersController::class, 'addUser']);
+Route::put('updateUser/{id}', [UsersController::class, 'updateUser']);
+Route::delete('deleteUser/{id}', [UsersController::class, 'deleteUser']);
+});
