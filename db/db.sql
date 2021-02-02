3c659c7d11cafe662ea8159052ad12d57834dd1d
Itzhak Franco <itzhakfranco@gmail.com>
2021-02-02 19:39:22 +0200

    cln up 990

--- db/db.sql
+++ db/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 02, 2021 at 04:01 PM
+-- Generation Time: Feb 02, 2021 at 05:35 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -25,8 +25,19 @@ CREATE TABLE `users` (
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `website` varchar(300) NOT NULL,
-  `phone` varchar(20) NOT NULL
+  `phone` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `website`, `phone`) VALUES
+(34, 'Leanne Graham', 'Sincere@april.biz', 'hildegard.org', '1-770-736-8031 x56442'),
+(35, 'Ervin Howell', 'Shanna@melissa.tv', 'anastasia.net', '010-692-6593 x09125'),
+(36, 'Clementine Bauch', 'Nathan@yesenia.net', 'ramiro.info', '1-463-123-4447'),
+(37, 'Patricia Lebsack', 'Julianne.OConner@kory.org', 'kale.biz', '493-170-9623 x156'),
+(38, 'Chelsey Dietrich', 'Lucio_Hettinger@annie.ca', 'demarco.info', '(254)954-1289');
 
 --
 -- Indexes for dumped tables
@@ -46,4 +57,4 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
